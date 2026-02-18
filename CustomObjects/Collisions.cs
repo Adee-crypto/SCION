1001c90e8d12c6efbd5427f202e714f9634a19a1
Benny Jiang <jiang.3192@buckeyemail.osu.edu>
2026-02-18 03:31:01 -0500

    opti

--- CustomObjects/Collisions.cs
+++ CustomObjects/Collisions.cs
@@ -7,52 +7,42 @@ public class Collisions
 {
     public static void ManageCollision(Player player, IEnumerable<Rectangle> objects, Vector2 movement, ref bool isGrounded, ref Vector2 velocity)
     {
-        Vector2 currentPos = player.Position;
+        Vector2 currentPosition = player.Position;
 
-        currentPos.X += movement.X;
-        player.Position = currentPos;
-        foreach (var o in objects)
+        currentPosition.X += movement.X;
+        player.Position = currentPosition;
+        foreach (var obj in objects)
         {
-            if (!player.Hitbox.Intersects(o)) continue;
+            if (!player.Hitbox.Intersects(obj)) continue;
             if (movement.X > 0)
-                currentPos.X = o.Left - player.Hitbox.Width;
+                currentPosition.X = obj.Left - player.Hitbox.Width;
             else if (movement.X < 0)
-                currentPos.X = o.Right;
-            player.Position = currentPos;
+                currentPosition.X = obj.Right;
         }
 
-        float oldY = currentPos.Y;
-        float movementY = movement.Y;
-        if (movementY == 0f && velocity.Y >= 0f)
+        currentPosition.Y += (int)movement.Y;
+        player.Position = currentPosition;
+        foreach (var obj in objects)
         {
-            movementY = 1f;
-        }
-        currentPos.Y += movementY;
-        player.Position = currentPos;
-        Rectangle newRect = player.Hitbox;
-        Rectangle oldRect = new Rectangle(newRect.X, (int)oldY, newRect.Width, newRect.Height);
-
-        foreach (var o in objects)
-        {
-            if (!newRect.Intersects(o)) continue;
-            if (oldRect.Bottom <= o.Top) // Landing on top
+            if (!player.Hitbox.Intersects(obj)) continue;
+            if (player.Hitbox.Bottom - (int)movement.Y < obj.Top) // Landing on top
             {
                 isGrounded = true;
-                currentPos.Y = o.Top - newRect.Height;
+                currentPosition.Y = obj.Top - player.Hitbox.Height;
                 velocity.Y = 0;
             }
-            else if (oldRect.Top >= o.Bottom) // Hitting ceiling
+            else if (player.Hitbox.Top - (int)movement.Y >= obj.Bottom) // Hitting ceiling
             {
-                currentPos.Y = o.Bottom;
+                currentPosition.Y = obj.Bottom;
                 velocity.Y = 0;
             }
             else // Pushed upward
             {
-                currentPos.Y = o.Bottom >= (o.Top - newRect.Height) ? (o.Top - newRect.Height) : o.Bottom;
+                currentPosition.Y = obj.Bottom >= (obj.Top - player.Hitbox.Height) ? (obj.Top - player.Hitbox.Height) : obj.Bottom;
             }
-            player.Position = currentPos;
-            newRect = player.Hitbox;
         }
+
+        player.Position = currentPosition;
     }
 
     public static bool CheckGrounded(Player player, IEnumerable<Rectangle> objects)
