ac899dfecc5ad6aa386b744cfaef5a28f528493b
Justin-OSU <211546887+Justin-OSU@users.noreply.github.com>
2026-02-26 15:50:24 -0500

    Fixed several warnings in PlantUtil and Consts.

--- Entities/Plants/PlantUtil.cs
+++ Entities/Plants/PlantUtil.cs
@@ -4,9 +4,9 @@ namespace Sprint2.Entities.Plants;
 
 public static class PlantUtil
 {
-    public static List<(int, int)> growDirs = [(0, 1), (0, -1), (1, 0), (-1, 0)];
+    public static List<(int, int)> growDirs { get; } = [(0, 1), (0, -1), (1, 0), (-1, 0)];
 
-    public static Dictionary<Species, float> SpeciesGrowTimes = new()
+    public static Dictionary<Species, float> SpeciesGrowTimes { get; } = new()
     {
         {Species.grass, 0.1f },
         {Species.apple, 0.2f },
